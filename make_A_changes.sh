04f32f576082546361ee6daf85324d6320c6721c
Andrew Pasquale <andrew@elytra.net>
2022-08-25 14:38:25 -0400

    Update Sweeter DNS also

--- make_A_changes.sh
+++ make_A_changes.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-IP_ADDR="23.185.0.4" 
-./change-rec.sh greatplains ${IP_ADDR}
-# ./change-rec.sh rmacc ${IP_ADDR}
-./change-rec.sh kycyberteam ${IP_ADDR}
-./change-rec.sh careers-ct ${IP_ADDR}
-# ./change-rec.sh sweeter ${IP_ADDR}
-# ./change-rec.sh mines ${IP_ADDR}
-# ./change-rec.sh trecis ${IP_ADDR}
-./change-rec.sh campuschampions ${IP_ADDR}
-./change-rec.sh coco ${IP_ADDR}
-./change-rec.sh usrse ${IP_ADDR}
-./change-rec.sh connect ${IP_ADDR}
+PANTHEON_IP_ADDR="23.185.0.4"
+./change-rec.sh greatplains ${PANTHEON_IP_ADDR}
+./change-rec.sh kycyberteam ${PANTHEON_IP_ADDR}
+./change-rec.sh careers-ct ${PANTHEON_IP_ADDR}
+./change-rec.sh campuschampions ${PANTHEON_IP_ADDR}
+./change-rec.sh coco ${PANTHEON_IP_ADDR}
+./change-rec.sh usrse ${PANTHEON_IP_ADDR}
+./change-rec.sh connect ${PANTHEON_IP_ADDR}
+
+WPI_IP_ADDR="130.215.45.247"
+./change-rec.sh sweeter ${WPI_IP_ADDR}
+./change-rec.sh mines ${WPI_IP_ADDR}
+./change-rec.sh trecis ${WPI_IP_ADDR}
+./change-rec.sh rmacc ${WPI_IP_ADDR}
